661203a6f784a2a972dd551a337d8b7b344368c9
tobiasvidelaug14 <tobiasvidelaug14@gmail.com>
2024-08-13 12:43:41 -0300

    Menu: cambio estético a los botones y agregado 'efecto hover' a los mismos

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ logo_UNViMe = pygame.image.load('img/LOGO-UNViMe-BLANCO-PIE.png')
 logo_UNViMe_rect = logo_UNViMe.get_rect()
 
 # Cargar y redimensionar las imágenes para los botones
-botones_ancho, botones_alto = 120, 120
+botones_ancho, botones_alto = 110, 110
 boton_juego1 = pygame.image.load('img/boton_juego1.png')
 boton_juego1 = pygame.transform.scale(boton_juego1, (botones_ancho, botones_alto))
 
@@ -54,10 +54,10 @@ def dibujar_menu():
   pantalla.blit(logo_UNViMe, logo_UNViMe_rect)
 
   # Dibujar botones con imágenes
-  boton1_rect = pantalla.blit(boton_juego1, (ANCHO // 3 - boton_juego1.get_width() // 2, 175))
-  boton2_rect = pantalla.blit(boton_juego2, (ANCHO - (ANCHO // 3) - boton_juego2.get_width() // 2, 175))
-  boton3_rect = pantalla.blit(boton_juego3, (ANCHO // 3 - boton_juego3.get_width() // 2, 325))
-  boton4_rect = pantalla.blit(boton_juego4, (ANCHO - (ANCHO // 3) - boton_juego4.get_width() // 2, 325))
+  boton1_rect = pantalla.blit(boton_juego1, (ANCHO // 2 - boton_juego1.get_width() * 1.25, 165))
+  boton2_rect = pantalla.blit(boton_juego2, (ANCHO // 2 + boton_juego2.get_width() * 0.25, 165))
+  boton3_rect = pantalla.blit(boton_juego3, (ANCHO // 2 - boton_juego3.get_width() * 1.25, 315))
+  boton4_rect = pantalla.blit(boton_juego4, (ANCHO // 2 + boton_juego4.get_width() * 0.25, 315))
 
   pygame.display.flip()
 
@@ -75,6 +75,33 @@ while True:
   else:
     pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)  # Volver al cursor normal
 
+  if boton1_rect.collidepoint(pos_mouse):
+    boton_juego1 = pygame.image.load('img/boton_juego1_hover.png')
+    boton_juego1 = pygame.transform.scale(boton_juego1, (botones_ancho, botones_alto))
+  else:
+    boton_juego1 = pygame.image.load('img/boton_juego1.png')
+    boton_juego1 = pygame.transform.scale(boton_juego1, (botones_ancho, botones_alto))
+
+  if boton2_rect.collidepoint(pos_mouse):
+    boton_juego2 = pygame.image.load('img/boton_juego2_hover.png')
+    boton_juego2 = pygame.transform.scale(boton_juego2, (botones_ancho, botones_alto))
+  else:
+    boton_juego2 = pygame.image.load('img/boton_juego2.png')
+    boton_juego2 = pygame.transform.scale(boton_juego2, (botones_ancho, botones_alto))
+
+  if boton3_rect.collidepoint(pos_mouse):
+    boton_juego3 = pygame.image.load('img/boton_juego3_hover.png')
+    boton_juego3 = pygame.transform.scale(boton_juego3, (botones_ancho, botones_alto))
+  else:
+    boton_juego3 = pygame.image.load('img/boton_juego3.png')
+    boton_juego3 = pygame.transform.scale(boton_juego3, (botones_ancho, botones_alto))
+
+  if boton4_rect.collidepoint(pos_mouse):
+    boton_juego4 = pygame.image.load('img/boton_juego4_hover.png')
+    boton_juego4 = pygame.transform.scale(boton_juego4, (botones_ancho, botones_alto))
+  else:
+    boton_juego4 = pygame.image.load('img/boton_juego4.png')
+    boton_juego4 = pygame.transform.scale(boton_juego4, (botones_ancho, botones_alto))
 
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
