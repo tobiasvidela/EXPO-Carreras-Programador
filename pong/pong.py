1035ba25208796d3506c3487d5db4cc6eca6cee7
tobiasvidelaug14 <tobiasvidelaug14@gmail.com>
2024-08-13 22:22:50 -0300

    feat: single player AI setted

--- pong/pong.py
+++ pong/pong.py
@@ -65,12 +65,110 @@ ball.center = (ANCHO / 2 - ball_ancho / 2, ALTO / 2 - - ball_alto / 2)
 x_speed, y_speed = 1, 1
 
 def Pong1P(jugando):
-  print('One player')
+
+  pong_screen = pygame.display.set_mode((ANCHO, ALTO))
+
+  # reset
+  p1.center = (distancia_borde , ALTO / 2)
+  p2.center = (ANCHO - distancia_borde, ALTO / 2)
+  p1_score, p2_score = 0, 0
+  ball.center = (ANCHO / 2 - ball_ancho / 2, ALTO / 2 - - ball_alto / 2)
+  x_speed, y_speed = random.choice([1, -1]), random.choice([1, -1])
+
+  # Bucle principal
+  while jugando:
+    
+    timer.tick(framerate)
+
+    keys_pressed = pygame.key.get_pressed()
+
+    if keys_pressed[pygame.K_w]:
+      if p1.top > 0:
+        p1.top -= player_speed
+    if keys_pressed[pygame.K_s]:
+      if p1.bottom < ALTO:
+        p1.bottom += player_speed
+    if p2.top < ball.y:
+      if p2.top > 0:
+        p2.top += player_speed * 0.71
+    if p2.bottom > ball.y:
+      if p2.bottom < ALTO:
+        p2.bottom -= player_speed * 0.71
+        
+    if keys_pressed[pygame.K_ESCAPE]:
+      jugando = False
+      # Reset window size and caption
+
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        pygame.quit()
+        sys.exit()
+        
+    # Rebote con bordes
+    if ball.y >= ALTO:
+      y_speed = -1
+      pygame.mixer.Sound.play(hit)
+    if ball.y <= 0:
+      y_speed = 1
+      pygame.mixer.Sound.play(hit)
+    # "Gol"
+    gol = False
+    if ball.x <= 0 - ball.width / 2:
+      pygame.mixer.Sound.play(point)
+      gol = True
+      p1_score += 1
+    if ball.x >= ANCHO - ball.width / 2:
+      pygame.mixer.Sound.play(point)
+      gol = True
+      p2_score += 1
+      
+    if gol:
+      pygame.time.delay(900)
+      ball.center = (ANCHO / 2 - ball_ancho / 2, ALTO / 2 - - ball_alto / 2)
+      x_speed, y_speed = random.choice([1, -1]), random.choice([1, -1])
+    
+    # Rebote con jugador
+    if p1.colliderect(ball) and x_speed < 0:
+      pygame.mixer.Sound.play(hit)
+      x_speed = 1
+    if p2.colliderect(ball) and x_speed > 0:
+      pygame.mixer.Sound.play(hit)
+      x_speed = -1
+    
+    p1_score_text = fuente.render(str(p1_score), True, BLANCO)
+    p2_score_text = fuente.render(str(p2_score), True, BLANCO)
+    
+    ball.x += x_speed * 2
+    ball.y += y_speed * 2
+
+    pong_screen.fill(NEGRO)
+
+    pygame.draw.rect(pong_screen, BLANCO, p1)
+    pygame.draw.rect(pong_screen, BLANCO, p2)
+    pygame.draw.circle(pong_screen, BLANCO, ball.center, ball_ancho)
+
+    pong_screen.blit(p1_score_text, (ANCHO // 2 + 100, 30))
+    pong_screen.blit(p2_score_text, (ANCHO // 2 - 100, 30))
+    
+    pygame.display.flip()
+    
+    # GANADOR
+    if p1_score == max_score or p2_score == max_score:
+      pygame.mixer.Sound.play(win)
+      # Loading screen
+      pygame.time.delay(1000)
+      p1_score, p2_score = 0, 0
+      p1.center = (distancia_borde , ALTO / 2)
+      p2.center = (ANCHO - distancia_borde, ALTO / 2)
+
+    pygame.display.flip()
 
 def Pong2P(jugando):
   pong_screen = pygame.display.set_mode((ANCHO, ALTO))
 
   # reset
+  p1.center = (distancia_borde , ALTO / 2)
+  p2.center = (ANCHO - distancia_borde, ALTO / 2)
   p1_score, p2_score = 0, 0
   ball.center = (ANCHO / 2 - ball_ancho / 2, ALTO / 2 - - ball_alto / 2)
   x_speed, y_speed = random.choice([1, -1]), random.choice([1, -1])
@@ -168,16 +266,19 @@ def dibujar_menu():
   titulo = fuente.render("Elige el modo de juego", True, BLANCO)
   pantalla.blit(titulo, (ANCHO // 2 - titulo.get_width() // 2, ALTO // 4))
   
-  opcion1 = fuente.render("1. Un jugador", True, BLANCO)
+  opcion1 = fuente.render("1. Single Player", True, BLANCO)
   pantalla.blit(opcion1, (ANCHO // 2 - opcion1.get_width() // 2, ALTO // 2 - 30))
   
-  opcion2 = fuente.render("2. Dos jugadores", True, BLANCO)
+  opcion2 = fuente.render("2. Two Players", True, BLANCO)
   pantalla.blit(opcion2, (ANCHO // 2 - opcion2.get_width() // 2, ALTO // 2 + 30))
+  
+  opcion3 = fuente.render("3. Volver", True, BLANCO)
+  pantalla.blit(opcion3, (ANCHO // 2 - opcion3.get_width() // 2, ALTO // 2 + 90))
   
   pygame.display.flip()
 
-def runPong(jugando):
-  while True:
+def runPong(jugando, menu):
+  while menu:
     dibujar_menu()
 
     for event in pygame.event.get():
@@ -191,7 +292,9 @@ def runPong(jugando):
         elif event.key == pygame.K_2:
           jugando = True
           Pong2P(jugando)
+        elif event.key == pygame.K_3:
+          menu = False
 
     pygame.display.update()
 
-runPong(jugando)
+runPong(jugando, True)
